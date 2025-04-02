b6e884806d07d306193185686eaa79826c55bc97
Infusion810 <infusionpvtltd@gmail.com>
2025-04-02 20:14:28 +0530

    Update marketLogicRoutes.js

--- Routes/marketLogicRoutes.js
+++ Routes/marketLogicRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 router.post('/laggai_khai_bets', placeBet);
-router.get('/laggai_khai_getuserbet/:userId', getUserBets);
+router.get('/laggai_khai_getuserbet/:userId/:match', getUserBets);
 router.get('/getwalletandexposure/:userId', getwalletandexposure);
 router.get('/admin/unique-matches-labels', getUniqueMatchesAndLabels);
 router.post('/admin/new-declaration', submitNewDeclaration);
