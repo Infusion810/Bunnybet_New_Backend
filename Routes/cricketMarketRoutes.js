46d9464b7f8757aea9ebb0d5d40408afae485eae
Infusion810 <infusionpvtltd@gmail.com>
2025-04-02 20:12:19 +0530

    Update cricketMarketRoutes.js

--- Routes/cricketMarketRoutes.js
+++ Routes/cricketMarketRoutes.js
@@ -4,8 +4,9 @@ const betController = require('../controller/cricketMarketController');
 
 router.post('/api/cricket-market/bets', betController.createBet);
 router.get('/api/cricket-market/getbets', betController.getAllBets);
-router.get('/api/cricket-market/:userId', betController.getBetsByUser);
+router.get('/api/cricket-market/:userId/:match', betController.getBetsByUser);
 router.get('/api/cricket-market-match/:matbet', betController.getBetsByMatch);
+router.get('/api/cricket-market-by-matchName/:matchName', betController.getBetsByMatchNameAndSession);
 router.get('/api/cricket-market/:id', betController.deleteBet);
 router.delete('/reset', betController.resetAllBet);
 module.exports = router;
