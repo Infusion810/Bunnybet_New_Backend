4a443712c522e0bbdacde591fcac076f90b63889
Infusion810 <infusionpvtltd@gmail.com>
2025-04-10 00:33:10 +0530

    Update pagesRoute.js

--- Routes/pagesRoute.js
+++ Routes/pagesRoute.js
@@ -1,20 +1,22 @@
 
 const express = require('express');
 
-const {cricketpageadmin,cricketpagesignup,titliadminlogin,matkapagelogin,avaitorpagesignup,aarpaarparchpagelogin,avaitoradminlogin,matkaadminlogin} = require('../controller/pagesControler');
+const {paymentpageadmin,titlipagesignup,cricketpageadmin,cricketpagesignup,titliadminlogin,matkapagelogin,avaitorpagesignup,aarpaarparchpagelogin,avaitoradminlogin,matkaadminlogin} = require('../controller/pagesControler');
 const router = express.Router();
 
+router.post('/admin/cricketpagesignup', cricketpagesignup); 
+router.post('/admin/titlipagesignup', titlipagesignup);
+router.post('/admin/signupmatka', matkapagelogin);   
+router.post('/admin/avaitorpagesignup', avaitorpagesignup);  
+router.post('/admin/aarpaarparchpagelogin', aarpaarparchpagelogin);  
 
-router.post('/admin/signupmatka', matkapagelogin);
-router.post('/admin/avaitorpagesignup', avaitorpagesignup);
-router.post('/admin/aarpaarparchpagelogin', aarpaarparchpagelogin);
-router.post('/page/login', avaitoradminlogin);
 router.post('/page/matka/login', matkaadminlogin);
-// router.post('/admin/cricketpagesignup', cricketpagesignup);
+
+router.post('/page/login', avaitoradminlogin);
 router.post('/page/tittli/login', titliadminlogin);
 router.post('/page/cricket/login', cricketpageadmin);
+router.post('/page/payment/login', paymentpageadmin);
 
-router.post('/admin/cricketpagesignup', cricketpagesignup);
 
 module.exports = router;
 
