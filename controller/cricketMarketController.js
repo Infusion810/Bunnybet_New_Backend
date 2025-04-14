c987b15ee9dcbf43400aed6afc209bac5b9317ad
Infusion810 <infusionpvtltd@gmail.com>
2025-04-15 00:12:59 +0530

    Update cricketMarketController.js

--- controller/cricketMarketController.js
+++ controller/cricketMarketController.js
@@ -64,12 +64,12 @@ exports.createBet = async (req, res) => {
                     const newProfitA = Math.abs(Number(profitA) || 0);
 
                     // const exposureUpdate = Math.abs(prevProfitA - newProfitA) + Math.abs(prevExposure-parsedExposure)-prevExposure;
-                    const exposureUpdate = Math.abs(prevExposure - newProfitA)-prevExposure;
+                    const exposureUpdate = Math.abs(prevProfitA - newProfitA)-prevExposure;
                     userWallet.exposureBalance += exposureUpdate;
                     userWallet.balance =balance;
                     await userWallet.save();
                     previousOppositeBet.result="cancel";
-                    previousOppositeBet.exposure=Math.abs(prevExposure - newProfitA);
+                    previousOppositeBet.exposure=Math.abs(prevProfitA - newProfitA);
                     await previousOppositeBet.save();
                     return res.status(201).json("Bet cancel each other");
                     console.log(`Wallet updated for same runs and opposite bet: ${userId}, Balance = ${userWallet.balance}, Exposure = ${userWallet.exposureBalance}`);
